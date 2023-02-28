8e386e72e8daa28b6b9735fe44501e17e7832e0f
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-02-28 01:01:53 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,7 @@ export const Header = ({
         <h1 className='text-2xl font-semibold '>{title}</h1>
         <p>{description}</p>
         <div className='w-72 flex flex-row gap-1 flex-wrap justify-center items-center '>
-          <img src='src/assets/icons8-html-5.svg' alt='html logo' width={30} />
+          <img src='../assets/icons8-html-5.svg' alt='html logo' width={30} />
           <img src='src/assets/icons8-css3.svg' alt='css3 logo' width={30} />
           <img
             src='src/assets/icons8-tailwindcss.svg'
