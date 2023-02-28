da182c9a4f4fda263a4bb3e64645d5c929f993a6
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-02-28 01:30:15 -0300

    🐛
      desolvendo bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,5 @@
 import { Iheader } from '../types/interface'
 
-// import next from 'src/assets/icons8-next.js.svg'
-
 export const Header = ({
   srcHeader,
   altHeader,
@@ -33,41 +31,18 @@ export const Header = ({
         <h1 className='text-2xl font-semibold '>{title}</h1>
         <p>{description}</p>
         <div className='w-72 flex flex-row gap-1 flex-wrap justify-center items-center '>
+          <img src='/icons8-html-5.svg' alt='html logo' width={30} />
+          <img src='/icons8-css3.svg' alt='css3 logo' width={30} />
           <img
-            src='../assets/icons8-html-5.svg'
-            alt='html logo'
-            width={30}
-            className='w-8'
-          />
-          <img
-            src='src/assets/icons8-css3.svg'
-            alt='css3 logo'
-            width={30}
-            className='w-8'
-          />
-          <img
-            src='src/assets/icons8-tailwindcss.svg'
+            src='/icons8-tailwindcss.svg'
             alt='tailwindcss logo'
-            // width={30}
-            className='w-8'
-          />
-          <img src='src/assets/icons8-sass.svg' alt='sass logo' width={30} />
-          <img
-            src='src/assets/icons8-javascript.svg'
-            alt='javascript logo'
             width={30}
           />
-          <img
-            src='src/assets/icons8-typescript.svg'
-            alt='typescript logo'
-            width={30}
-          />
-          <img
-            src={'src/assets/icons8-react.svg'}
-            alt='react logo'
-            width={30}
-          />
-          {/* <img src={next} alt='next logo' width={30} /> */}
+          <img src='/icons8-sass.svg' alt='sass logo' width={30} />
+          <img src='/icons8-javascript.svg' alt='javascript logo' width={30} />
+          <img src='/icons8-typescript.svg' alt='typescript logo' width={30} />
+          <img src='/icons8-react.svg' alt='react logo' width={30} />
+          <img src='/icons8-next.js.svg' alt='next logo' width={30} />
         </div>
       </div>
     </div>
