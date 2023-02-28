95a0c52838a58d0a2c806e7003920bb5129ed2c7
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-02-28 00:52:40 -0300

    🎨
      add imagens de tecnolog e nova descricao

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
-import { Iheader } from '../types/interface';
+import { Iheader } from '../types/interface'
 
-export const Header = ({srcHeader, altHeader, title, description}: Iheader) => {
+export const Header = ({
+  srcHeader,
+  altHeader,
+  title,
+  description
+}: Iheader) => {
   return (
     <div
-    className='
+      className='
       h-1/2
       flex
       flex-col
@@ -14,23 +19,40 @@ export const Header = ({srcHeader, altHeader, title, description}: Iheader) => {
     '
     >
       <img
-      className='
+        className='
       rounded-full
       w-36
-      ' 
-      src={srcHeader} 
-      alt={altHeader} 
+      '
+        src={srcHeader}
+        alt={altHeader}
       />
 
-      <div 
-      className='text-center max-w-ms '
-      >
-        <h1
-        className='text-2xl font-semibold '
-        >{title}</h1>
+      <div className='w-72 text-center max-w-ms '>
+        <h1 className='text-2xl font-semibold '>{title}</h1>
         <p>{description}</p>
+        <div className='w-72 flex flex-row gap-1 flex-wrap justify-center items-center '>
+          <img src='src/assets/icons8-html-5.svg' alt='html logo' width={30} />
+          <img src='src/assets/icons8-css3.svg' alt='css3 logo' width={30} />
+          <img
+            src='src/assets/icons8-tailwindcss.svg'
+            alt='tailwindcss logo'
+            width={30}
+          />
+          <img src='src/assets/icons8-sass.svg' alt='sass logo' width={30} />
+          <img
+            src='src/assets/icons8-javascript.svg'
+            alt='javascript logo'
+            width={30}
+          />
+          <img
+            src='src/assets/icons8-typescript.svg'
+            alt='typescript logo'
+            width={30}
+          />
+          <img src='src/assets/icons8-react.svg' alt='react logo' width={30} />
+          <img src='src/assets/icons8-next.js.svg' alt='next logo' width={30} />
+        </div>
       </div>
-      
     </div>
   )
 }
